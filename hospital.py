ffdd94017c63325a7f52ef6e1cb0bcf562e209fe
Mateusz Groszewski <neonowa.vi@gmail.com>
2024-02-06 22:27:01 +0100

    added sort

--- hospital.py
+++ hospital.py
@@ -12,6 +12,23 @@ class Hospital:
         newPatient.append(newpatient.patient_age)
         newPatient.append(newpatient.patient_disease)
         Patient_DB.patients.append(newPatient)
+
+    def remove_patient(ID):
+        Patient_DB.patients.pop(ID)
+
+    def sort_patietns(sorttype):
+        match int(sorttype):
+            case 1:
+                Patient_DB.patients.sort(key= lambda x: x[0])
+            case 2:
+                Patient_DB.patients.sort(reverse = True, key= lambda x: x[0])
+            case 3:
+                Patient_DB.patients.sort(key= lambda x: x[1])
+            case 4:
+                Patient_DB.patients.sort(reverse = True, key= lambda x: x[1])
+        Hospital.show_patients()
+
+
     def show_patients ():
         for i in range(len(Patient_DB.patients)):
             print(f'ID: {i} | Name: {Patient_DB.patients[i][0]} | Last name: {Patient_DB.patients[i][1]} | Gender: {Patient_DB.patients[i][2]} | Age: {Patient_DB.patients[i][3]} | Diseases: {Patient_DB.patients[i][4]}')
@@ -21,4 +38,5 @@ class Hospital:
 
 Hospital.add_patient(Patient("Maryla","Rodowicz","F",86,["Kaszel","Grypa"]))
 Hospital.add_patient(Patient("Antos","Miarodajny","M",23,["Astma","Goraczka"]))
-Hospital.add_patient(Patient("karol","Drzidarol","M",32,["Biegunka","Katar"]))
+Hospital.add_patient(Patient("Karol","Drzidarol","M",32,["Biegunka","Katar"]))
+Hospital.add_patient(Patient("Zbigniew","Dziura","M",49,["Owsiki","Katar"]))
--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 from hospital import Hospital
 from patient import Patient
+from patient_db import Patient_DB
 
 def showMenu():
     choice = int
@@ -16,9 +17,13 @@ def showMenu():
     0. Exit program
     """
             )
-        choice = input("Choice: ")
-        match choice:
-            case "1":
+        try:
+            choice = int(input("Choice: "))
+        except ValueError:
+            print("Wrong input! Try again")
+            continue
+        match int(choice):
+            case 1:
 
 
                 diseases = []
@@ -28,7 +33,14 @@ def showMenu():
                 name = input("Name: ")
                 lastname = input("Last name: ")
                 gender = input("Gender(M/F): ")
-                age = int(input("Age: "))
+                if (gender != "M" and gender != "F"):
+                    print("Invalid gender! Expected 'M' or 'F'")
+                    continue
+                try:
+                    age = int(input("Age: "))
+                except ValueError:
+                    print("Invalid age!")
+                    continue
                 print("Enter diseases, enter '0' to stop adding!")
                 while True:
                     disease = input("Disease: ")
@@ -40,16 +52,47 @@ def showMenu():
 
 
                 
-            case "2":
-                print("remove patient")
-            case "3":
-                print("sort patients")
-            case "4":
+            case 2:
+                print("""       
+       REMOVE PATIENT
+    ====================""")
+                print("Enter ID number of patient that you want to remove")
+                try:
+                    ID = int(input("Enter ID: "))
+                    if (ID > len(Patient_DB.patients)-1):
+                        print("Invalid ID")
+                    else:
+                        Hospital.remove_patient(ID)
+                except ValueError:
+                    print("Invalid ID")
+                    continue
+
+            case 3:
+                print("""       
+        SORT PATIENTS
+    ====================""")
+                print("""
+        1. Sort by name A-Z
+        2. Sort by name Z-A
+        3. Sort by last name A-Z
+        4. Sort by last name Z-A
+""")    
+                try: 
+                    sorttype = int(input("Enter your choice: "))
+                    if (sorttype > 4 or sorttype < 1):
+                        print("Invalid value!")
+                    else:
+                        Hospital.sort_patietns(sorttype)
+                except ValueError:
+                    print("Invalid value!")
+                    continue        
+        
+            case 4:
                 print("""       
         SHOW PATIENTS
     ====================""")
                 Hospital.show_patients()
-            case "0":
+            case 0:
                 print("Exiting program")
                 break
             case other:
